a9733c0c9dfbcbc46ec628db7cb975b53f99b214
DucanhPH1906 <anhndph19496@fpt.edu.vn>
2022-12-15 22:27:30 +0700

    duc anh tes khuyen mai chi tiet

---
+++ test/Itespake.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+
+import repository.*;
+import java.util.List;
+import model. KhuyenMai;
+import model.KhuyenMaiChiTiet;
+/**
+ *
+ * @author adm
+ */
+public interface Itespake {
+    List<KhuyenMaiChiTiet> selectAllChiKhuyenMais();
+}
